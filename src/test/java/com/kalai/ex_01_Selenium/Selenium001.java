27605aba4abe3649fe69b766c9b1edab5bab8dd4
AMANI <kalaiara@gmail.com>
2025-08-26 19:24:51 -0500

    commit

---
+++ src/test/java/com/kalai/ex_01_Selenium/Selenium001.java
@@ -0,0 +1,12 @@
+package com.kalai.ex_01_Selenium;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Selenium001 {
+
+    @Test
+    public void test_Code(){
+        Assert.assertEquals("Pramod","Pramod");
+    }
+}
